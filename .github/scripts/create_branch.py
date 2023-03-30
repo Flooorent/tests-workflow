34471d40a1d737d3aabee0ad84dca20c208c0a73
Florent Moiny <florent.moiny@gmail.com>
2023-03-30 13:51:58 +0200

    Release/v0.0.14 (#26)
    * Release/v0.0.8 (#19)
    
    * try squashing
    
    * another commit
    
    * add workflow to create PRs against dev and main
    
    * test github rest api (#24)
    
    * update readme
    
    * use head branch instead of manually computing tag version

--- .github/scripts/create_branch.py
+++ .github/scripts/create_branch.py
@@ -1,3 +1,11 @@
+"""
+Example call:
+python create_branch.py \
+--repo Flooorent/tests-workflow \
+--gh-token TOKEN \
+--sha ed776f17e289cbec69b2a41defad35816dd70608 \
+--tag v0.0.13
+"""
 import argparse
 import requests
 from requests.exceptions import HTTPError
@@ -10,24 +18,67 @@ def get_owner_and_repo(full_repo_name: str) -> (str, str):
     return owner, repo
 
 
+def check_if_is_release_branch(branch_name: str) -> bool:
+    return branch_name.startswith("release/v")
+
+
+def check_if_is_hotfix_branch(branch_name: str) -> bool:
+    return branch_name.startswith("hotfix/v")
+
+
+def get_tag_from_release(release: str) -> str:
+    prefix = "release/"
+    assert release.startswith(f"{prefix}v"), f"A release should be of the form '{prefix}vx.y.z'."
+    return release.strip(prefix)
+
+
+def get_tag_from_hotfix(hotfix: str) -> str:
+    prefix = "hotfix/"
+    assert hotfix.startswith(f"{prefix}v"), f"A hotfix should be of the form '{prefix}vx.y.z'."
+    return hotfix.strip(prefix)
+
+
+def get_tag_version(branch_name: str) -> str:
+    is_release_branch = check_if_is_release_branch(branch_name)
+    is_hotfix_branch = check_if_is_hotfix_branch(branch_name)
+
+    assert is_release_branch or is_hotfix_branch, f"branch_name {branch_name} should be a release or a hotfix branch."
+
+    tag_version = ""
+
+    if is_release_branch:
+        tag_version = get_tag_from_release(branch_name)
+
+    if is_hotfix_branch:
+        tag_version = get_tag_from_hotfix(branch_name)
+
+    return tag_version
+
+
 parser = argparse.ArgumentParser(description="test")
 
 parser.add_argument("--repo", help="GitHub full repository name (including the owner)", type=str)
 parser.add_argument("--gh-token", help="GitHub token to call the REST API", type=str)
 parser.add_argument("--sha", help="Commit sha used to create the branch", type=str)
-parser.add_argument("--tag", help="Name of the tag to use, must be of the form 'vx.y.z'", type=str)
+parser.add_argument(
+    "--head-branch",
+    help="Name of the head branch to use, must be of the form 'release/v*' or 'hotfix/v*'.",
+    type=str,
+)
 
 args = parser.parse_args()
 
+is_release_branch = check_if_is_release_branch(args.head_branch)
+is_hotfix_branch = check_if_is_hotfix_branch(args.head_branch)
+
+if not (is_release_branch or is_hotfix_branch):
+    raise ValueError(f"Head branch {args.head_branch} must be a release or a hotfix branch.")
+
 owner, repo = get_owner_and_repo(args.repo)
 url = f"https://api.github.com/repos/{owner}/{repo}/git/refs"
-target_branch_name = f"test-dev/{args.tag}"
 
-# owner = "Flooorent"
-# repo = "tests-workflow"
-# gh_token = "TODO"
-# commit sha of release/v0.0.10
-# sha = "ed776f17e289cbec69b2a41defad35816dd70608"
+tag_version = get_tag_version(args.head_branch)
+target_branch_name = f"test-dev/{tag_version}"
 
 headers = {
   "Accept": "application/vnd.github+json",
